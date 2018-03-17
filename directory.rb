4727fa28a039a78487cabf5ae24779fd731e7aa4
Patryk Pilecki <ppilecki@icloud.com>
2018-03-17 12:30:33 +0000

    user input introduced

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit the return twice"
+  #empty array to  start with
+  students = []
+  #let's get it from the user
+  name = gets.chomp
+  #setting the loop if there is one provided
+  while !name.empty? do
+    #hash hash babe
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #give me more names
+    name = gets.chomp
+  end
+  #and the ready product array of input_students
+  students
+end
 
 def print_header
 puts "The students of Villains Academy"
@@ -24,6 +29,7 @@ end
 def print_footer(students)
 puts "Overall, we have #{students.count} great students"
 end
+students = input_students
 print_header
 print(students)
 print_footer(students)
